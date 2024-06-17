7f77c2de9fa8ca709a0e37587bd97087bdac6be5
benzebra <filippobrajucha@gmail.com>
2024-06-17 10:44:19 +0200

    first commit

---
+++ FL/client.py
@@ -0,0 +1,85 @@
+import argparse
+import warnings
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import log_loss
+from sklearn.model_selection import train_test_split
+
+import flwr as fl
+import utils
+from flwr_datasets import FederatedDataset
+
+import pandas as pd
+
+PATH_TRAIN_X = "../UCI_HAR_Dataset/train/X_train.txt"
+PATH_TRAIN_Y = "../UCI_HAR_Dataset/train/y_train.txt"
+
+PATH_TEST_X = "../UCI_HAR_Dataset/test/X_test.txt"
+PATH_TEST_Y = "../UCI_HAR_Dataset/test/y_test.txt"
+
+PATH_TRAIN_SBJ = "../UCI_HAR_Dataset/train/subject_train.txt"
+
+PATH_TEST_SBJ = "../UCI_HAR_Dataset/test/subject_test.txt"
+
+PATH_FT = "../UCI_HAR_Dataset/features.txt"
+features = pd.read_csv(PATH_FT, sep=" ", header=None, index_col=0).reset_index()
+
+if __name__ == "__main__":
+    N_CLIENTS = 30
+
+    parser = argparse.ArgumentParser(description="Flower")
+    parser.add_argument(
+        "--partition-id",
+        type=int,
+        choices=range(0, N_CLIENTS),
+        required=True,
+        help="Specifies the artificial data partition",
+    )
+    args = parser.parse_args()
+    partition_id = args.partition_id
+
+    # Load the partition data
+    df_x_train = pd.read_fwf(PATH_TRAIN_X, header=None)
+    df_x_train.rename(columns=features[1], inplace=True)
+
+    y_train_col = pd.read_fwf(PATH_TRAIN_Y, header=None)
+    print(f"x_train shape: {df_x_train.shape}\ny_train shape: {y_train_col.shape}")
+    
+    # SPLITTING
+    X_train, X_test, y_train, y_test = train_test_split(df_x_train, y_train_col, random_state=42, test_size=0.3)
+    print(f"X_train shape: {X_train.shape}\nX_test shape: {X_test.shape}\ny_train shape: {y_train.shape}\ny_test shape: {y_test.shape}")
+
+    # Create LogisticRegression Model
+    model = LogisticRegression(
+        penalty="l2",
+        max_iter=1,  # local epoch
+        warm_start=True,  # prevent refreshing weights when fitting
+    )
+
+    # Setting initial parameters, akin to model.compile for keras models
+    utils.set_initial_params(model)
+
+    # Define Flower client
+    class MnistClient(fl.client.NumPyClient):
+        def get_parameters(self, config):  # type: ignore
+            return utils.get_model_parameters(model)
+
+        def fit(self, parameters, config):  # type: ignore
+            utils.set_model_params(model, parameters)
+            # Ignore convergence failure due to low local epochs
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                model.fit(X_train, y_train)
+            print(f"Training finished for round {config['server_round']}")
+            return utils.get_model_parameters(model), len(X_train), {}
+
+        def evaluate(self, parameters, config):  # type: ignore
+            utils.set_model_params(model, parameters)
+            loss = log_loss(y_test, model.predict_proba(X_test))
+            accuracy = model.score(X_test, y_test)
+            return loss, len(X_test), {"accuracy": accuracy}
+
+    # Start Flower client
+    fl.client.start_client(
+        server_address="0.0.0.0:8080", client=MnistClient().to_client()
+    )
