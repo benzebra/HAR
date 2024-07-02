d3fa3d05daace399eb63e20c22ee2a8150ba94ba
benzebra <filippobrajucha@gmail.com>
2024-07-02 18:05:19 +0200

    fl-tensorflow improvements

--- FL-tensorflow/server.py
+++ FL-tensorflow/server.py
@@ -1,4 +1,4 @@
-# import flwr as fl
+import flwr as fl
 
 # fl.server.start_server(config=fl.server.ServerConfig(num_rounds=3))
 from typing import List, Tuple
@@ -19,8 +19,18 @@ def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
 
 
 # Define strategy
-strategy = FedAvg(evaluate_metrics_aggregation_fn=weighted_average)
-
+# strategy = FedAvg(evaluate_metrics_aggregation_fn=weighted_average)
+strategy = fl.server.strategy.FedAvg(
+    fraction_fit=0.1,                   # Sample 10% of available clients for training
+    fraction_evaluate=0.05,             # Sample 5% of available clients for evaluation
+    min_fit_clients=3,                  # Never sample less than 10 clients for training
+    min_evaluate_clients=3,             # Never sample less than 5 clients for evaluation
+    min_available_clients=int(
+        3
+    ),                                  # Wait until at least 75 clients are available
+    evaluate_metrics_aggregation_fn=weighted_average,  # aggregates federated metrics
+    # evaluate_fn=get_evaluate_fn(centralized_testset),  # global evaluation function
+)
 
 # Define config
 config = ServerConfig(num_rounds=3)
